f5a15c11ecba765bb25f953c5759308105bc83d3
UltraChris64 <48795813+UltraChris64@users.noreply.github.com>
2021-06-03 08:42:42 -0400

    First

---
+++ script.js
@@ -0,0 +1,10 @@
+function goHome(){
+  // console.log('hi')
+  location.replace("https://food-bank-replit.danielcorso.repl.co/index.html");
+}
+function goAbout(){
+  location.replace("https://food-bank-replit.danielcorso.repl.co/about.html");
+}
+function goContact(){
+  location.replace("https://food-bank-replit.danielcorso.repl.co/contact.html");
+}
